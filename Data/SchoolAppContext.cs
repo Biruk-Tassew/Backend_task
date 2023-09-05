c772cd51fc0813e0696491eae0e6285d01a2821e
fasika <fasikafikadu38@gmail.com>
2023-09-05 15:22:22 +0300

    add student model

--- Data/SchoolAppContext.cs
+++ Data/SchoolAppContext.cs
@@ -3,12 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
-  public class SchoolAppContext : DbContext
-  {
-    public SchoolAppContext(DbContextOptions<SchoolAppContext> options) : base(options)
+    public class SchoolAppContext : DbContext
     {
-    }
+        public SchoolAppContext(DbContextOptions<SchoolAppContext> options) : base(options)
+        {
+        }
 
-    public DbSet<Teacher> Teachers { get; set; }
-  }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; } // Add DbSet for Student model
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Optional: Configure any additional relationships or constraints
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.Teacher)
+                .WithMany()
+                .HasForeignKey(s => s.TeacherId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
 }
--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -1,12 +1,15 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
-  public class Teacher
-  {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Subect { get; set; }
-  }
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+
+        public ICollection<Student> Students { get; set; } // Navigation property for the one-to-many relationship
+    }
 }
